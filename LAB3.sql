ed5880571fee8d3e95a9ac81230ce1f332ba93ee
Ticcccc <118881696+Ticcccc@users.noreply.github.com>
2022-11-23 23:37:29 +0700

    Add files via upload

---
+++ LAB3.sql
@@ -0,0 +1,101 @@
+﻿--LAB3:
+Bài 1:
+➢Với mỗi đề án, liệt kê tên đề án và tổng số giờ làm việc một tuần của tất cả các nhân viên tham dự đề án đó.
+o Xuất định dạng “tổng số giờ làm việc” kiểu decimal với 2 số thập phân.
+SELECT TENDEAN, CAST(SUM(THOIGIAN) AS decimal(5,2)) AS 'tổng số giờ làm việc' FROM CONGVIEC
+INNER JOIN DEAN ON CONGVIEC.MADA=DEAN.MADA
+INNER JOIN PHANCONG ON CONGVIEC.MADA=PHANCONG.MADA
+GROUP BY TENDEAN
+o Xuất định dạng “tổng số giờ làm việc” kiểu varchar
+SELECT TENDEAN, CONVERT(varchar, SUM(THOIGIAN)) AS 'tổng số giờ làm việc' FROM CONGVIEC
+INNER JOIN DEAN ON CONGVIEC.MADA=DEAN.MADA
+INNER JOIN PHANCONG ON CONGVIEC.MADA=PHANCONG.MADA
+GROUP BY TENDEAN
+--Với mỗi phòng ban, liệt kê tên phòng ban và lương trung bình của những nhân viên làm việc cho phòng ban đó.
+o Xuất định dạng “luong trung bình” kiểu decimal với 2 số thập phân, sử dụng dấu phẩy để phân biệt phần nguyên và phần thập phân.
+SELECT TENPHG, CAST(AVG(LUONG) as decimal(10,2)) AS 'Lương trung bình' from NHANVIEN
+INNER JOIN PHONGBAN ON NHANVIEN.PHG=PHONGBAN.MAPHG
+GROUP BY TENPHG
+o Xuất định dạng “luong trung bình” kiểu varchar. Sử dụng dấu phẩy tách cứ mỗi 3 chữ số trong chuỗi ra, gợi ý dùng thêm các hàm Left, Replace
+SELECT TENPHG, LEFT(CAST(AVG(LUONG) as varchar(10)), 3)+','
++REPLACE(CAST(AVG(LUONG) as varchar(10)), LEFT(CAST(AVG(LUONG) as varchar(10)), 3), ',' )
+AS 'Lương trung bình' from NHANVIEN
+INNER JOIN PHONGBAN ON NHANVIEN.PHG=PHONGBAN.MAPHG
+GROUP BY TENPHG
+Bài 2:
+➢Với mỗi đề án, liệt kê tên đề án và tổng số giờ làm việc một tuần của tất cả các nhân viên tham dự đề án đó.
+o Xuất định dạng “tổng số giờ làm việc” với hàm CEILING
+SELECT TENDEAN, CEILING(CAST(SUM(THOIGIAN) AS decimal(5,2))) AS 'tổng số giờ làm việc' FROM CONGVIEC
+INNER JOIN DEAN ON CONGVIEC.MADA=DEAN.MADA
+INNER JOIN PHANCONG ON CONGVIEC.MADA=PHANCONG.MADA
+GROUP BY TENDEAN
+o Xuất định dạng “tổng số giờ làm việc” với hàm FLOOR
+SELECT TENDEAN, FLOOR(CAST(SUM(THOIGIAN) AS decimal(5,2))) AS 'tổng số giờ làm việc' FROM CONGVIEC
+INNER JOIN DEAN ON CONGVIEC.MADA=DEAN.MADA
+INNER JOIN PHANCONG ON CONGVIEC.MADA=PHANCONG.MADA
+GROUP BY TENDEAN
+o Xuất định dạng “tổng số giờ làm việc” làm tròn tới 2 chữ số thập phân
+SELECT TENDEAN, ROUND(CAST(SUM(THOIGIAN) AS decimal(5,2)), 2) AS 'tổng số giờ làm việc' FROM CONGVIEC
+INNER JOIN DEAN ON CONGVIEC.MADA=DEAN.MADA
+INNER JOIN PHANCONG ON CONGVIEC.MADA=PHANCONG.MADA
+GROUP BY TENDEAN
+--Cho biết họ tên nhân viên (HONV, TENLOT, TENNV) có mức lương trên mức lương trung bình (làm tròn đến 2 số thập phân) của phòng "Nghiên cứu"
+SELECT LUONG, (HONV+' '+TENLOT+' '+TENNV+' ') AS 'Họ tên NV có mức lương trung bình' FROM NHANVIEN
+WHERE LUONG>
+(SELECT ROUND(AVG(LUONG), 2) FROM NHANVIEN, PHONGBAN
+WHERE PHG=MAPHG AND TENPHG=N'Nghiên cứu')
+Bài 3:
+➢Danh sách những nhân viên (HONV, TENLOT, TENNV, DCHI) có trên 2 thân nhân, thỏa các yêu cầu
+o Dữ liệu cột HONV được viết in hoa toàn bộ
+SELECT UPPER(HONV) AS 'Nhân viên có trên 2 thân nhân', TENLOT, TENNV, DCHI
+FROM NHANVIEN, THANNHAN
+WHERE NHANVIEN.MANV=THANNHAN.MA_NVIEN
+GROUP BY UPPER(HONV), TENLOT, TENNV, DCHI
+HAVING COUNT(THANNHAN.MA_NVIEN) > 2
+o Dữ liệu cột TENLOT được viết chữ thường toàn bộ
+SELECT UPPER(HONV) AS 'TENNV', LOWER(TENLOT) AS 'TENLOT', TENNV, DCHI
+FROM NHANVIEN, THANNHAN
+WHERE NHANVIEN.MANV=THANNHAN.MA_NVIEN
+GROUP BY UPPER(HONV), LOWER(TENLOT), TENNV, DCHI
+HAVING COUNT(THANNHAN.MA_NVIEN) > 2
+o Dữ liệu chột TENNV có ký tự thứ 2 được viết in hoa, các ký tự còn lại viết thường( ví dụ: kHanh)
+SELECT UPPER(HONV) AS 'TENNV', LOWER(TENLOT) AS 'TENLOT', 
+LOWER(LEFT(TENNV, 1))+UPPER(SUBSTRING(TENNV, 2, 1))+LOWER(SUBSTRING(TENNV, 3, LEN(TENNV))), DCHI
+FROM NHANVIEN, THANNHAN
+WHERE NHANVIEN.MANV=THANNHAN.MA_NVIEN
+GROUP BY UPPER(HONV), LOWER(TENLOT), 
+LOWER(LEFT(TENNV, 1))+UPPER(SUBSTRING(TENNV, 2, 1))+LOWER(SUBSTRING(TENNV, 3, LEN(TENNV))), DCHI
+HAVING COUNT(THANNHAN.MA_NVIEN) > 2
+o Dữ liệu cột DCHI chỉ hiển thị phần tên đường, không hiển thị các thông tin khác như số nhà hay thành phố.
+SELECT UPPER(HONV) AS 'TENNV', LOWER(TENLOT) AS 'TENLOT', 
+LOWER(LEFT(TENNV, 1))+UPPER(SUBSTRING(TENNV, 2, 1))+LOWER(SUBSTRING(TENNV, 3, LEN(TENNV))), 
+SUBSTRING(DCHI, CHARINDEX(' ', DCHI)+1, CHARINDEX(',', DCHI) - CHARINDEX(' ', DCHI)-1)
+FROM NHANVIEN, THANNHAN
+WHERE NHANVIEN.MANV=THANNHAN.MA_NVIEN
+GROUP BY UPPER(HONV), LOWER(TENLOT), 
+LOWER(LEFT(TENNV, 1))+UPPER(SUBSTRING(TENNV, 2, 1))+LOWER(SUBSTRING(TENNV, 3, LEN(TENNV))), 
+SUBSTRING(DCHI, CHARINDEX(' ', DCHI)+1, CHARINDEX(',', DCHI) - CHARINDEX(' ', DCHI)-1)
+HAVING COUNT(THANNHAN.MA_NVIEN) > 2
+➢Cho biết tên phòng ban và họ tên trưởng phòng của phòng ban có đông nhân viên nhất, hiển thị thêm một cột thay thế tên trưởng phòng bằng tên “Fpoly”
+DECLARE @THONGKE TABLE (MAP INT, MANVTP INT, TK INT)
+INSERT INTO @THONGKE
+SELECT PHG, MA_NQL, COUNT(MANV) FROM NHANVIEN GROUP BY PHG, MA_NQL
+DECLARE @MAX INT
+SELECT @MAX = MAX(TK) FROM @THONGKE
+SELECT TENPHG, HONV+' '+TENLOT+' '+TENNV AS 'Họ tên NV', HONV+' '+TENLOT+' Fpoly' AS 'Họ tên FPOLY' FROM PHONGBAN
+A INNER JOIN (SELECT *FROM @THONGKE WHERE TK=@MAX) B ON A.MAPHG = B.MAP
+INNER JOIN NHANVIEN C ON C.MANV = B.MANVTP
+Bài 4:
+➢Cho biết các nhân viên có năm sinh trong khoảng 1960 đến 1965.
+SELECT * FROM NHANVIEN
+WHERE YEAR(NGSINH)
+BETWEEN 1960 AND 1965
+➢Cho biết tuổi của các nhân viên tính đến thời điểm hiện tại.
+SELECT YEAR(GETDATE())-YEAR(NGSINH) AS 'Tuổi', TENNV FROM NHANVIEN
+➢Dựa vào dữ liệu NGSINH, cho biết nhân viên sinh vào thứ mấy.
+SELECT DATENAME(WEEKDAY, NGSINH) AS 'Thứ', TENNV FROM NHANVIEN
+➢Cho biết số lượng nhân viên, tên trưởng phòng, ngày nhận chức trưởng phòng và ngày nhận chức trưởng phòng hiển thi theo định dạng dd-mm-yy (ví dụ 25-04-2019)
+SELECT TRPHG, HONV+' '+TENLOT+' '+TENNV AS 'Họ tên TP', CONVERT(VARCHAR, NG_NHANCHUC, 105) AS 'Ngày nhận chức', SL AS 'SLNV'
+FROM PHONGBAN
+A INNER JOIN (SELECT PHG, COUNT(MANV) AS SL FROM NHANVIEN GROUP BY PHG) B ON A.MAPHG=B.PHG
+INNER JOIN NHANVIEN C ON A.TRPHG=C.MANV
---
+++ LAB4.sql
@@ -0,0 +1,111 @@
+﻿--LAB4:
+Bài 1:
+➢Viết chương trình xem xét có tăng lương cho nhân viên hay không. Hiển thị cột thứ 1 là TenNV, cột thứ 2 nhận giá trị
+o “TangLuong” nếu lương hiện tại của nhân viên nhở hơn trung bình lương trong
+phòng mà nhân viên đó đang làm việc.
+o “KhongTangLuong “ nếu lương hiện tại của nhân viên lớn hơn trung bình lương
+trong phòng mà nhân viên đó đang làm việc.
+SELECT IIF(LUONG>=LTB,'Không tăng lương','Tăng lương')
+AS THUONG, TENNV, LUONG, LTB
+FROM
+(SELECT TENNV,LUONG,LTB FROM NHANVIEN,
+(SELECT PHG,AVG(LUONG) AS 'LTB' FROM NHANVIEN GROUP BY PHG) AS TEMP
+WHERE NHANVIEN.PHG=TEMP.PHG) AS ABC
+➢Viết chương trình phân loại nhân viên dựa vào mức lương.
+o Nếu lương nhân viên nhỏ hơn trung bình lương mà nhân viên đó đang làm việc thì xếp loại “nhanvien”, ngược lại xếp loại “truongphong”
+SELECT IIF(LUONG<=LTB,'NhanVien','TruongPhong')
+AS PL,TENNV,LUONG,LTB
+FROM 
+(SELECT TENNV,LUONG,LTB FROM NHANVIEN,
+(SELECT PHG,AVG(LUONG) AS 'LTB' FROM NHANVIEN GROUP BY PHG) AS TEMP
+WHERE NHANVIEN.PHG=TEMP.PHG) AS ABC
+➢Viết chương trình hiển thị TenNV như hình bên dưới, tùy vào cột phái của nhân viên
+SELECT TENNV = CASE PHAI
+WHEN 'nam' then 'Mr. '+[TENNV]
+WHEN N'Nữ' then 'Ms. '+[TENNV]
+END
+FROM NHANVIEN
+➢Viết chương trình tính thuế mà nhân viên phải đóng theo công thức:
+o 0<luong<25000 thì đóng 10% tiền lương
+o 25000<luong<30000 thì đóng 12% tiền lương
+o 30000<luong<40000 thì đóng 15% tiền lương
+o 40000<luong<50000 thì đóng 20% tiền lương
+o Luong>50000 đóng 25% tiền lương
+SELECT TENNV,LUONG,THUE=CASE
+WHEN LUONG BETWEEN 0 AND 25000 THEN LUONG*0.1
+WHEN LUONG BETWEEN 25000 AND 30000 THEN LUONG*0.12
+WHEN LUONG BETWEEN 30000 AND 40000 THEN LUONG*0.15
+WHEN LUONG BETWEEN 40000 AND 50000 THEN LUONG*0.2
+ELSE LUONG*0.25
+END 
+FROM NHANVIEN
+--Bài 2:
+➢Cho biết thông tin nhân viên (HONV, TENLOT, TENNV) có MaNV là số chẵn.
+declare @dem int=2
+while @dem<(select count(MANV) from NHANVIEN)
+	begin
+		select HONV,TENLOT,TENNV,MANV from NHANVIEN WHERE CAST(MANV as int)=@dem
+		set @dem=@dem+2
+	end
+➢Cho biết thông tin nhân viên (HONV, TENLOT, TENNV) có MaNV là số chẵn nhưng không tính nhân viên có MaNV là 4.
+declare @dem int=2
+while @dem<(select count(MANV) from NHANVIEN)
+	begin
+		if @dem=4
+		begin
+			set @dem=@dem+2
+			continue
+		end
+		if @dem%2=0
+			select HONV,TENLOT,TENNV,MANV from NHANVIEN WHERE CAST(MANV as int)=@dem
+			set @dem=@dem+2
+	end
+--Bài 3:
+➢ Thực hiện chèn thêm một dòng dữ liệu vào bảng PhongBan theo 2 bước
+o Nhận thông báo “ thêm dư lieu thành cong” từ khối Try
+o Chèn sai kiểu dữ liệu cột MaPHG để nhận thông báo lỗi “Them dư lieu that bai”
+từ khối Catch
+SELECT * FROM PHONGBAN
+BEGIN TRY
+	INSERT INTO PHONGBAN(TENPHG,MAPHG,TRPHG,NG_NHANCHUC)
+	VALUES(N'MARKETING', 7, '009', '2022-11-23')
+	PRINT N'Thêm dữ liệu thành công'
+END TRY
+BEGIN CATCH
+	PRINT N'Thêm dữ liệu thất bại'
+END CATCH
+➢ Viết chương trình khai báo biến @chia, thực hiện phép chia @chia cho số 0 và dùng RAISERROR để thông báo lỗi.
+BEGIN TRY
+	declare @chia int;
+	set @chia = 55 / 0;
+END TRY
+BEGIN CATCH
+	declare @ErrorMessage nvarchar(2048), @ErrorSeverity int, @ErrorState int;
+	select @ErrorMessage = ERROR_MESSAGE(),
+			@ErrorSeverity = ERROR_SEVERITY(),
+			@ErrorState = ERROR_STATE();
+	RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
+END CATCH
+--Bài 4: GV cho thêm
+➢ Viết chương trình tính tổng các số chẵn từ 1 tới 10.
+DECLARE @TONG int=0, @DEM int=1
+WHILE(@DEM<=10)
+BEGIN
+	SET @TONG = @TONG+@DEM
+	SET @DEM = @DEM+1
+END
+PRINT @TONG
+➢ Viết chương trình tính tổng các số chẵn từ 1 tới 10 nhưng bỏ số 4.
+DECLARE @TONG int=0, @DEM int=1
+WHILE(@DEM<=10)
+BEGIN
+	IF @DEM=4
+		BEGIN
+			SET @DEM=@DEM+1
+			CONTINUE
+		END	
+	IF @DEM!=4
+		SET @TONG = @TONG+@DEM
+		SET @DEM = @DEM+1
+END
+PRINT @TONG
